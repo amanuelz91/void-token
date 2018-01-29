a3d2fce5bf277f6d792afe04249e498d322f6853
Eric <3th3r3um@users.noreply.github.com>
2018-01-29 08:40:20 -0500

    Add network configs to truffle.js. Fixes #8. (#15)

--- truffle.js
+++ truffle.js
@@ -1,4 +1,28 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const fs = require('fs');
+
+const mnemonic = process.env.MNEMONIC;
+
 module.exports = {
-  // See <http://truffleframework.com/docs/advanced/configuration>
-  // to customize your Truffle configuration!
+  migrations_directory: "./migrations",
+  networks: {
+    development: {
+      host: "localhost",
+      port: 8545,
+      network_id: "*", // Match any network id
+      gas: 5000000
+    },
+    rinkeby: {
+      provider: new HDWalletProvider(mnemonic, 'https://rinkeby.infura.io'),
+      network_id: '*',
+      gas: 500000,
+      gasPrice: 5000000000
+    },
+    mainnet: {
+      provider: new HDWalletProvider(mnemonic, 'https://mainnet.infura.io'),
+      network_id: 1,
+      gas: 5000000, // FIXME: Estimate gas.
+      gasPrice: 4000000000
+    }
+  }
 };
