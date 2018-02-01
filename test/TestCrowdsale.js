2b58a6220b6a6a3623565f2a1a8eb0de2b73c07f
Eric <3th3r3um@users.noreply.github.com>
2018-02-01 02:58:05 -0500

    Add more tests for Crowdsale.sol. Fixes #29. (#30)

--- test/TestCrowdsale.js
+++ test/TestCrowdsale.js
@@ -1,7 +1,11 @@
 const TokenMock = artifacts.require('Token');
 const CrowdsaleMock = artifacts.require('Crowdsale');
 const BigNumber = web3.BigNumber;
+const Crypto = require("crypto");
 
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
 
 contract('Crowdsale', function(accounts) {
 
@@ -104,8 +108,73 @@ contract('Crowdsale', function(accounts) {
         'Incorrect number of tokens after purchasing tokens.'
       );
     });
+  });
 
-    it('should increment wallet balance with ether when tokens are purchased');
+  describe('Wallet', function() {
+    beforeEach('deploy new CrowdsaleMock', async () => {
+
+      // Generate a fake address to ensure intial wallet balance is always zero.
+      // web3.js 1.0 includes an accounts.create() method to use instead.
+      let empty_wallet = '0x' + Crypto.randomBytes(20).toString('hex');
+
+      // Deploy crowdsale contract.
+      contract = await CrowdsaleMock.new(empty_wallet, RATE, CAP, {gas: 3000000});
+      // Deploy token contract.
+      token = await TokenMock.new(NAME, SYMBOL, DECIMALS, INITIAL_SUPPLY, {gas: 3000000});
+      // Assign token contract to crowdsale.
+      await contract.assignTokenContract(token.address);
+      // Add crowdsale contract address to token contract.
+      await token.addOwner(contract.address);
+    });
+
+    it('should increment wallet ether balance when tokens are purchased.', async function(){
+      const amount_ether = 2.3642;
+      const token_buyer = accounts[3];
+      const token_recipient = accounts[7];
+      const value = new BigNumber(web3.toWei(amount_ether, 'ether'));
+      const wallet = await contract.wallet();
+      // Make sure initial wallet balance is zero.
+      const wallet_balance_initial = web3.eth.getBalance(wallet);
+      assert.equal(wallet_balance_initial.valueOf(), 0, 'Initial wallet balance is not zero.');
+      // Purchase tokens.
+      await contract.buyTokens(token_recipient, {from: token_buyer, value: value});
+      // Get wallet balance.
+      const wallet_balance_final = await web3.eth.getBalance(wallet);
+      assert.equal(
+        value.valueOf(),
+        wallet_balance_final.valueOf(),
+        'Incorrect wallet balance after purchasing tokens.'
+      );
+    });
+
+    it('should increment wallet ether balance for multiple purchases.', async function(){
+      const number_of_purchases = 7;
+      const max_amount_ether = 4;
+      const token_recipient = '0x5304a4158b232CE32AaA228Ed4355A18C4251E82';
+      let wallet_balance_expected = new BigNumber(0);
+
+      for(let i = 0; i < number_of_purchases; i++){
+         // Get random amount of ether.
+        const random_amount_ether = getRandomArbitrary(0, max_amount_ether);
+        const value = new BigNumber(web3.toWei(random_amount_ether));
+        // Get random account from accounts[].
+        const token_buyer = accounts[Math.floor(Math.random()*accounts.length)];
+        // Purchase tokens.
+        await contract.buyTokens(token_recipient, {from: token_buyer, value: value});
+        // Add value to wallet_balance_expected.
+        wallet_balance_expected = wallet_balance_expected.add(value);
+      }
+
+      // Get wallet balance.
+      const wallet = await contract.wallet();
+      const wallet_balance_actual = await web3.eth.getBalance(wallet);
+
+      assert.equal(
+        wallet_balance_actual.valueOf(),
+        wallet_balance_expected.valueOf(),
+        'Incorrect wallet ether balance after multiple token purchases.'
+      );
+    });
   });
 
   describe('Hardcap', function() {
@@ -154,6 +223,29 @@ contract('Crowdsale', function(accounts) {
         number_of_tokens.valueOf(),
         'Total supply was not incremented correctly after buying tokens.'
       );
+    });
+
+    it('should ensure hasEnded() is true after hardcap is reached.', async function(){
+      const cap = await contract.cap();
+      const rate = await contract.rate();
+      const token_buyer = accounts[6];
+      const token_recipient = accounts[3];
+      // Buy as many tokens as possible within the hardcap.
+      await contract.buyTokens(token_recipient, {from: token_buyer, value: cap.valueOf()});
+      has_ended = await contract.hasEnded();
+      assert.isTrue(has_ended, 'hasEnded() is false after the hardcap is reached.');
+    });
+
+    it('should ensure hasEnded() is false when hardcap has not been reached.', async function(){
+      const cap = await contract.cap();
+      const value = cap.sub(1); // Subtract one wei.
+      const rate = await contract.rate();
+      const token_buyer = accounts[6];
+      const token_recipient = accounts[3];
+      // Buy as many tokens as possible within the hardcap.
+      await contract.buyTokens(token_recipient, {from: token_buyer, value: value.valueOf()});
+      has_ended = await contract.hasEnded();
+      assert.isFalse(has_ended, 'hasEnded() is true before the hardcap is reached.');
     });
 
     it('should not allow to exceed hard cap', async function(){
